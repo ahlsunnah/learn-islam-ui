998aa1a1d733e7d2279c22b9282a4ce4c47ef1f8
abumalick <abdullah.hilson@gmail.com>
2017-11-05 10:42:01 +0000

    feat: sidebar in chapter pages fixes #5

--- src/templates/Chapter.jsx
+++ src/templates/Chapter.jsx
@@ -1,53 +1,119 @@
 // @flow
-import React from 'react'
+import * as React from 'react'
 import Helmet from 'react-helmet'
-import Link from 'gatsby-link'
+import styled from 'react-emotion'
+import FaBars from 'react-icons/lib/fa/bars'
+import GoMoveLeft from 'react-icons/lib/go/move-left'
+import ChapterSidebar from '../components/ChapterSidebar'
 
+const SIDEBAR_WIDTH = '18rem'
+
+const StyledChapterSidebar = styled(ChapterSidebar)`
+  width: ${props => (props.isSideBarVisible ? SIDEBAR_WIDTH : '0')};
+  transition: width 0.5s ease;
+`
+const ContentWrapper = styled.div`
+  flex: 1;
+  margin-left: ${props => (props.isSideBarVisible ? SIDEBAR_WIDTH : '0')};
+  transition: margin 0.5s ease;
+`
+const ChapterContent = styled.div`
+  p {
+    font-size: 1.25rem;
+  }
+`
 type Props = {
   data: {
-    contentfulChapter: Object,
+    contentfulChapter: {
+      title: string,
+      content: {
+        childMarkdownRemark: {
+          html: string,
+        },
+      },
+      course: {
+        chapter: Array<{
+          order: number,
+          slug: string,
+          title: string,
+        }>,
+        slug: string,
+        section: {
+          slug: string,
+          title: string,
+        },
+        title: string,
+      },
+    },
   },
   pathContext: {
     languagePath: string,
     slug: string,
   },
 }
-const Chapter = ({data, pathContext}: Props) => {
-  const {contentfulChapter: chapter} = data
-  const {languagePath, slug} = pathContext
-  return (
-    <div className="">
-      <Helmet title={`${chapter.title}`} />
-      <div className="tc">
-        <h1>{chapter.title}</h1>
-        {chapter.content && (
-          <div
-            dangerouslySetInnerHTML={{
-              __html: chapter.content.childMarkdownRemark.html,
-            }}
-          />
-        )}
-        {// NAV
-        chapter.course.chapter
-          .slice()
-          .sort((a, b) => a.order - b.order) // TODO: sort in the query ?
-          .map(navChapter => (
-            <div key={navChapter.slug}>
-              <Link
-                to={`${languagePath}${chapter.course.section.slug}/${chapter
-                  .course.slug}/${navChapter.slug}`}
+type State = {
+  isSideBarVisible: boolean,
+}
+
+class Chapter extends React.Component<Props, State> {
+  state = {
+    isSideBarVisible: true,
+  }
+  toggleSidebar: Function = () => {
+    this.setState(({isSideBarVisible}) => ({
+      isSideBarVisible: !isSideBarVisible,
+    }))
+  }
+  render() {
+    const {data, pathContext} = this.props
+    const {isSideBarVisible} = this.state
+    const {contentfulChapter: chapter} = data
+    const {languagePath} = pathContext
+    return (
+      <div className="flex">
+        <Helmet title={`${chapter.title}`} />
+        <StyledChapterSidebar
+          course={chapter.course}
+          languagePath={languagePath}
+          isSideBarVisible={isSideBarVisible}
+        />
+        <ContentWrapper
+          className="flex flex-column items-center"
+          isSideBarVisible={isSideBarVisible}
+        >
+          <div className="w-100 relative flex justify-center items-center bg-moon-gray">
+            <div className="absolute top-0 bottom-0 left-0 flex items-center">
+              <button
+                className="ml2 pa2 bg-transparent"
+                onClick={this.toggleSidebar}
               >
-                {navChapter.title}
-              </Link>
+                {isSideBarVisible ? (
+                  <GoMoveLeft className="f3" />
+                ) : (
+                  <FaBars className="f3" />
+                )}
+              </button>
             </div>
-          ))}
-        {/* <div
+            <h1 className="mv2 tc" css={`flex: 2;`}>
+              {chapter.title}
+            </h1>
+            <div />
+          </div>
+          {chapter.content && (
+            <ChapterContent
+              dangerouslySetInnerHTML={{
+                __html: chapter.content.childMarkdownRemark.html,
+              }}
+            />
+          )}
+          {/* <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{__html: section.html}}
         /> */}
+        </ContentWrapper>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Chapter
@@ -67,6 +133,7 @@ export const pageQuery = graphql`
         title
         section {
           slug
+          title
         }
         chapter {
           title

eaea0ce070b988f08fee3460abb415e948dc9dff
abumalick <abdullah.hilson@gmail.com>
2017-11-20 10:43:39 +0000

    feat: remove redux took for production

--- src/createStore.js
+++ src/createStore.js
@@ -14,12 +14,13 @@ const initialState = {count: 0}
 
 /* eslint no-underscore-dangle: 0 */
 const createStore = () =>
-  reduxCreateStore(
-    reducer,
-    initialState,
-    // TODO: Remove redux devtools in production
-    window &&
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-  )
+  process.env.NODE_ENV === 'production'
+    ? reduxCreateStore(reducer, initialState)
+    : reduxCreateStore(
+        reducer,
+        initialState,
+        window &&
+          window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__(),
+      )
 export default createStore

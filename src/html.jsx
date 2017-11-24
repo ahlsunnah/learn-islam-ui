cdfeabc84c077300d25055f9f74c6e51ab0369c5
abumalick <abdullah.hilson@gmail.com>
2017-11-24 10:33:25 +0000

    Inline css into javascript (it is the normal way in gatsby)

--- src/html.jsx
+++ src/html.jsx
@@ -1,38 +1,56 @@
 // @flow
 import * as React from 'react'
 
+let stylesStr
+if (process.env.NODE_ENV === `production`) {
+  try {
+    stylesStr = require(`!raw-loader!../public/styles.css`) // eslint-disable-line
+  } catch (e) {
+    console.log(e)
+  }
+}
+
 // const BUILD_TIME = new Date().getTime()
 type Props = {
   body: string,
   headComponents: React.Node,
   postBodyComponents: React.Node,
 }
-const HTML = ({body, headComponents, postBodyComponents}: Props) => (
-  <html lang="ar">
-    <head>
-      <meta charSet="UTF-8" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-      <meta name="viewport" content="width=device-width,initial-scale=1" />
-      <title>title</title>
-      <meta name="author" content="name" />
-      <meta name="description" content="description here" />
-      <meta name="keywords" content="keywords,here" />
-      {process.env.NODE_ENV === 'production' && (
-        <link rel="stylesheet" href="/styles.css" type="text/css" />
-      )}
+const HTML = ({body, headComponents, postBodyComponents}: Props) => {
+  let css
+  if (process.env.NODE_ENV === `production`) {
+    css = (
+      <style
+        id="gatsby-inlined-css"
+        dangerouslySetInnerHTML={{__html: stylesStr}}
+      />
+    )
+  }
+  return (
+    <html lang="ar">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>Learn islam</title>
+        <meta name="author" content="name" />
+        <meta name="description" content="description here" />
+        <meta name="keywords" content="keywords,here" />
 
-      {/* <link
+        {/* <link
           rel="shortcut icon"
           href="favicon.ico"
           type="image/vnd.microsoft.icon"
         /> */}
-      {headComponents}
-    </head>
-    <body>
-      <div id="___gatsby" dangerouslySetInnerHTML={{__html: body}} />
-      {postBodyComponents}
-    </body>
-  </html>
-)
+        {headComponents}
+        {css}
+      </head>
+      <body>
+        <div id="___gatsby" dangerouslySetInnerHTML={{__html: body}} />
+        {postBodyComponents}
+      </body>
+    </html>
+  )
+}
 
 export default HTML

eaa76ca1d45d333e69ed4eac967dfeba95ea294b
abumalick <abdullah.hilson@gmail.com>
2018-05-12 17:55:53 +0200

    fix(quiz): fix bugs 2

--- src/modules/Quizs/index.js
+++ src/modules/Quizs/index.js
@@ -1,13 +1,11 @@
 // @flow
 import * as React from 'react'
+import sandBg from '../../images/sand-bg.jpg'
 
 type Props = {}
 const QuizsContainer = (props: Props) => (
   <div>
-    <div
-      className="absolute h-100 w-100 "
-      style={{backgroundImage: srcsandBg}}
-    />
+    <div className="absolute h-100 w-100 " style={{backgroundImage: sandBg}} />
     TEST
   </div>
 )

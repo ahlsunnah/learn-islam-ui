6cfddfd52239aff421fdffbd09066ec51a1b6224
abumalick <abdullah.hilson@gmail.com>
2018-05-12 06:31:29 +0200

    feat(plugins): add necessary plugins with quick configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,35 @@ if (!API || !USER || !PASSWORD) {
 module.exports = {
   siteMetadata: {
     title: 'Learn Islam',
+    siteUrl: `https://madrassaminhaj.com`,
   },
   plugins: [
-    // 'gatsby-plugin-catch-links',
+    'gatsby-plugin-webpack-bundle-analyzer',
+    'gatsby-plugin-resolve-src',
+    'gatsby-plugin-accessibilityjs',
+    {
+      resolve: 'gatsby-plugin-sentry',
+      options: {
+        dsn: 'https://37ac93a8f3644b2fad4a3594da09c668@sentry.io/1205429',
+        version: '3.25.1',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // color: `tomato`,
+        showSpinner: false,
+      },
+    },
     'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://madrassaminhaj.com`,
+      },
+    },
     'gatsby-plugin-emotion',
+    'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-sass',
       options: {includePaths: [path.resolve(__dirname, './node_modules')]},
@@ -49,20 +73,20 @@ module.exports = {
         ],
       },
     },
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     path: `${__dirname}/src/pages`,
-    //     name: 'pages',
-    //   },
-    // },
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     path: `${__dirname}/content/`,
-    //     name: `content`,
-    //   },
-    // },
     'gatsby-plugin-netlify',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Madrassa Minhaj',
+        short_name: 'Madrassa Minhaj',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'minimal-ui',
+        icon: 'src/images/logo-square-1500.png',
+      },
+    },
+    'gatsby-plugin-offline',
+    `gatsby-plugin-netlify`,
   ],
 }

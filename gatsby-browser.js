e5e1d955467fe5695751b7827040f339ea317656
abumalick <abdullah.hilson@gmail.com>
2018-06-11 14:17:31 +0200

    chore(flow): upgrade and update flow-typed definitions

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,10 +3,10 @@
 import 'babel-polyfill'
 import * as React from 'react'
 import {Provider} from 'react-redux'
-import {Router} from 'react-router-dom' // eslint-disable-line
+import {Router} from 'react-router-dom'
 import createStore from './src/createStore'
 
-exports.replaceRouterComponent = ({history}) => {
+exports.replaceRouterComponent = ({history}: {history: {}}) => {
   const store = createStore()
 
   const ConnectedRouterWrapper = ({children}: {children: React.Node}) => (

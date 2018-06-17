81af556b7412393977352a3eff7ebf478dfe35ae
abumalick <abdullah.hilson@gmail.com>
2018-06-17 20:26:56 +0200

    refactor(chapter): use real css

--- src/modules/Chapter/StepContent.jsx
+++ src/modules/Chapter/StepContent.jsx
@@ -7,22 +7,17 @@ type Props = {
   title: string,
 }
 const StepContent = ({cta, title = '', content = ''}: Props) => (
-  <div className="ph6-ns ph4">
+  <div className="ph6-l ph5 ph4">
     <div className="flex justify-between items-center">
       <h2 className="blue">{title}</h2>
       {cta}
     </div>
     <div
-      className="ba bw1 br2 b--light-gray ph4-ns ph3 pv2"
-      style={{overflowY: 'scroll', height: '400px'}}
-    >
-      <p
-        className="f5 black-90"
-        dangerouslySetInnerHTML={{
-          __html: content,
-        }}
-      />
-    </div>
+      className="h400px ph4-ns ph3 pv2 ba bw1 br2 b--light-gray overflow-y-scroll f5 black-90"
+      dangerouslySetInnerHTML={{
+        __html: content,
+      }}
+    />
   </div>
 )
 

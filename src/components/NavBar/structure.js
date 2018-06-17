d778a0443b97121133cca2abf1a1dd29366575b2
abumalick <abdullah.hilson@gmail.com>
2018-06-17 20:39:09 +0200

    feat(nav): remove courses link

--- src/components/NavBar/structure.js
+++ src/components/NavBar/structure.js
@@ -1,11 +1,10 @@
-import AboutUs from 'react-icons/lib/md/info'
 import Tracks from 'react-icons/lib/fa/map-signs'
-import Courses from 'react-icons/lib/md/ondemand-video'
+import AboutUs from 'react-icons/lib/md/info'
 
 const items = [
   {title: 'aboutUs', link: 'aboutus', Icon: AboutUs},
   {title: 'tracks', link: 'masar', Icon: Tracks},
-  {title: 'courses', link: 'dourous', Icon: Courses},
+  // {title: 'courses', link: 'dourous', Icon: Courses},
 ]
 
 export default items

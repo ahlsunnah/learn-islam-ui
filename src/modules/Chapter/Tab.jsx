672f05ad755a6a330ff4f073f24acecc3888fdff
abumalick <abdullah.hilson@gmail.com>
2018-07-01 19:57:49 +0200

    refactor(chapter): tabs colors

--- src/modules/Chapter/Tab.jsx
+++ src/modules/Chapter/Tab.jsx
@@ -11,21 +11,24 @@ type Props = {
 }
 const Tab = ({active, children, handleClick, type}: Props) => (
   <div
-    className={cx('flex1', {
-      'bb bw1 b--color-black': active,
+    className={cx('flex1 bb bw1 ', {
+      'b--black': active,
+      'b--transparent': !active,
     })}
   >
     <button
       className={cx(
         'w-100 pv0 bn hover-only-bg-black-10 b--transparent pointer outline-0',
-        {
-          'black-80 ': active,
-        },
       )}
       name={type}
       onClick={handleClick}
     >
-      <div className={cx('pv3 flex items-center justify-center ')}>
+      <div
+        className={cx('pv3 flex items-center justify-center', {
+          'black-80': active,
+          'black-60': !active,
+        })}
+      >
         <TabIcon className="h2 mh0 mh1-l" height="20px" type={type} />
         <span className="mh1 dn db-l">{children}</span>
       </div>

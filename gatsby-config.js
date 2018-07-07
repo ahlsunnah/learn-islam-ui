432224f00827c950c04afb57e3c240eaffedf432
abumalick <abdullah.hilson@gmail.com>
2018-07-07 09:44:50 +0200

    feat: switch to react 16

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   plugins: [
     // 'gatsby-plugin-webpack-bundle-analyzer',
+    'gatsby-plugin-react-next',
     'gatsby-plugin-resolve-src',
     'gatsby-plugin-accessibilityjs',
     {

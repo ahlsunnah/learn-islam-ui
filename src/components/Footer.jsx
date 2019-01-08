760372814b55b1a48b44fd9d49bd66f14f144ddd
abumalick <abdullah.hilson@gmail.com>
2019-01-08 07:52:36 +0000

    fix: change order to have tachyons as last

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 // @flow
 import React from 'react'
 import 'styles/custom.scss'
-import 'tachyons/css/tachyons.min.css'
+// import 'tachyons/css/tachyons.min.css'
 
 const Footer = () =>
-  process.env.NODE_ENV === 'production' ? null : (
+   (
     <link
       rel="stylesheet"
       href="https://unpkg.com/tachyons@4.9.1/css/tachyons.min.css"

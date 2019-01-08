a386259e1d71b8a8cf3aa476c9604eaf7f776f3a
abumalick <abdullah.hilson@gmail.com>
2019-01-08 08:23:51 +0000

    fix: try to include tachyons later in the app

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,10 @@ import React from 'react'
 import 'styles/custom.scss'
 // import 'tachyons/css/tachyons.min.css'
 
-const Footer = () =>
-   (
-    <link
-      rel="stylesheet"
-      href="https://unpkg.com/tachyons@4.9.1/css/tachyons.min.css"
-    />
-  )
+const Footer = () => (
+  <link
+    rel="stylesheet"
+    href="https://unpkg.com/tachyons@4.9.1/css/tachyons.min.css"
+  />
+)
 export default Footer

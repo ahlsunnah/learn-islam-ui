65a71d1a47e767548e65873a849209a833b0e493
abumalick <abdullah.hilson@gmail.com>
2019-05-27 11:14:28 +0000

    feat: improve config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
-    // 'airbnb',
     'prettier',
     'prettier/react',
     'plugin:react/recommended',
@@ -14,57 +13,32 @@ module.exports = {
       jsx: true,
     },
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react-hooks'],
   env: {
     browser: true,
     node: true,
   },
-  globals: {
-    graphql: true,
-  },
   rules: {
-    // 'arrow-body-style': 0,
-    // camelcase: 0,
-    // 'jsx-a11y/anchor-is-valid': [
-    //   'error',
-    //   {
-    //     components: ['Link'],
-    //     specialLink: ['to'],
-    //   },
-    // ],
-    // 'jsx-a11y/label-has-for': 0,
-    // 'jsx-a11y/media-has-caption': 0,
-    // 'no-underscore-dangle': 0,
     'no-unexpected-multiline': 2,
-    // 'no-unused-vars': 1,
-    // 'prefer-arrow-callback': 0,
-    // 'react/destructuring-assignment': 0,
-    // 'react/forbid-prop-types': 0,
-    // 'react/jsx-wrap-multilines': 0,
-    // 'react/no-array-index-key': 0,
-    // 'react/no-danger': 0,
-    // 'react/no-unused-prop-types': 1,
-    // 'react/require-default-props': 0,
-    // 'react/self-closing-comp': 0,
+    'react/prop-types': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/array-type': 0,
     '@typescript-eslint/camelcase': 0,
     '@typescript-eslint/indent': 0,
+    '@typescript-eslint/interface-name-prefix': [1, 'always'],
+    '@typescript-eslint/member-delimiter-style': 0,
+    '@typescript-eslint/no-unused-vars': 1,
     '@typescript-eslint/no-var-requires': 0,
-    '@typescript-eslint/member-delimiter-style': ['error',
-    "multiline": {
-      "delimiter": "none",
-      "requireLast": false
-  },
-  "singleline": {
-      "delimiter": "comma",
-      "requireLast": true
-  },
-  ],
   },
   settings: {
     'import/resolver': {
       node: {
         paths: ['src'],
       },
+    },
+    react: {
+      version: 'detect',
     },
   },
 }

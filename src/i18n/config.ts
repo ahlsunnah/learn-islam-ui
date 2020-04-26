35b9f0eeb522dc792ec4347301627aca7836eb36
Mehdi C <mehdi@dev.com>
2020-04-26 11:21:42 +0200

    fix(i18Config) : add language detection from localstorage for i18n

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,12 +1,34 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+const localStorageName = 'LearnIslam-i18nextLng'
+
+const languageDetectorOptions = {
+  // order and from where user language should be detected
+  order: ['localStorage'],
+
+  // keys or params to lookup language from
+  lookupLocalStorage: localStorageName,
+
+  // cache user language on
+  caches: ['localStorage'],
+  excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
+
+  // only detect languages that are in the whitelist
+  checkWhitelist: false,
+}
 
 i18n
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    detection: languageDetectorOptions,
     fallbackLng: 'ar',
     debug: process.env.NODE_ENV === 'development',
     resources: {
@@ -17,7 +39,7 @@ i18n
         translations: require('../locales/fr.json'),
       },
     },
-    lng: 'ar',
+    lng: window.localStorage[localStorageName] || 'ar',
     ns: ['translations'],
     defaultNS: 'translations',
     returnObjects: true,

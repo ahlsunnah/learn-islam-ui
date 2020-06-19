e4b5dc99743a47c1e160dd84ccea16e41e7210c7
a72o <abumalick0@gmail.com>
2020-06-19 17:02:01 +0100

    refactor: remove unused types

--- src/types/chapter.ts
+++ src/types/chapter.ts
@@ -1,15 +1,5 @@
-import { IPageContext } from './index'
-import { TChapterQueryQuery } from '../graphqlTypes'
-
 export interface INext {
   path: string
   title: string
   type: 'quiz' | 'tracks' | 'course' | 'chapter'
-}
-
-export interface IChapterProps {
-  data?: TChapterQueryQuery
-  pageContext?: IPageContext & {
-    next: INext
-  }
 }
--- src/types/navbar.ts
+++
@@ -1,7 +0,0 @@
-export interface INavBarTranslations {
-  aboutUs: string
-  locale: string
-  localePath: string
-  siteName: string
-  tracks: string
-}
